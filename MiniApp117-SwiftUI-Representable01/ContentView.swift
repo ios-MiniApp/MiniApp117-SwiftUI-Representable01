db8bb59ef31a37246e74554549e63cc37fd7b7c9
Kota <maedakoutaa@gmail.com>
2022-12-25 15:38:25 +0900

    commit

--- MiniApp117-SwiftUI-Representable01/ContentView.swift
+++ MiniApp117-SwiftUI-Representable01/ContentView.swift
@@ -6,18 +6,42 @@
 //
 
 import SwiftUI
+import UIKit
 
 struct ContentView: View {
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        VStack(alignment: .leading) {
+            AddTextLabelRepresentable(text: "やっほー")
         }
         .padding()
     }
+
 }
+
+struct AddTextLabelRepresentable: UIViewRepresentable {
+
+    typealias UIViewType = UILabel
+    let text: String
+
+    init(text: String) {
+        print(#function)
+        self.text = text
+    }
+
+    func makeUIView(context: Context) -> UILabel {
+        print(#function)
+        return UILabel(frame: CGRect(x: 12, y: 12, width: 12, height: 12))
+    }
+
+    func updateUIView(_ uiView: UILabel, context: Context) {
+        print(#function)
+        uiView.text = text + "だよ"
+    }
+
+}
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
